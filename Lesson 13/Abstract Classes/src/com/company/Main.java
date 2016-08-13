def1e5346c5129b19be904889eb419f501ae839c
Judaco <judacossa@gmail.com>
2016-08-13 19:15:57 +0300

    Abstract Class
    לא ניתן ליצור אובייקט של המחלקה.
    ניתן לכתוב שיטה מופשטת בלי לכתוב בה קוד, והשיטה מיועדת להתממש ע"י מחלקה
    יורשת כלשהי.
    השימוש במילה abstract יגדיר לנו את המחלקה המופשטת או השיטה המופשטת.
    למשל דבר כזה קורה ב-shape
    
    בשורה התחתונה זו מחלקה שיוצרים במכוון (תכנון מראש), כדי שיירשו ממנה -
    יתבע בה inheritance

---
+++ Lesson 13/Abstract Classes/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal a = new Animal();//ein mimush
+    }
+}
+
+abstract class Animal(){//no code
+
+}
