46925f6f57dcb1a74fcca8d0ba76a0b858f58b29
judaco <judacossa@gmail.com>
2016-08-24 19:29:13 +0300

    Solution

--- Lesson 16/Generic and Collections/src/com/company/MyList2.java
+++ Lesson 16/Generic and Collections/src/com/company/MyList2.java
@@ -3,82 +3,95 @@ package com.company;
 /**
  * Created by Juda on 23/08/2016.
  */
-public class MyList2 implements  Listable {
+public class MyList1 implements Listable {
 
-    Link anchor;
-    Link last;
-    int pos;
+    private int[] nums;
+    private int pos;
 
-    public MyList2(){
-        anchor = new Link(10);
-        last = anchor;
+
+    public MyList1(){
+        nums = new int[10];
         pos = 0;
-    }
 
+    }
 
     @Override
     public void add(int num) {
-        /*
-        Link last = anchor;
-        while(last.next != null){
-            last = last.next;
-        }
-        */
-        last.next = new Link(num);
-        last = last.next;
-        pos++;
+        makeRoom();
+        nums[pos++] = num;
     }
+
+    private void makeRoom(){
+        if(pos == nums.length){//out of space
+            int[] temp = new int[nums.length * 2];
+            for (int i = 0; i < nums.length; i++) {
+                temp[i] = nums[i];
+            }
+            nums = temp;
+        }
+    }
+
+
 
     @Override
     public void remove(int index) {
         if(index < 0 || index >= pos)
-            throw new IndexOutOfBoundsException();
-        Link theOneBefore = anchor;
-        for (int i = 0; i < index; i++) {
-            theOneBefore = theOneBefore.next;
+            throw new IndexOutOfBoundsException("trying to remove at " + index);
+        for (int i = index; i < nums.length - 1; i++) {
+            nums[i] = nums[i+1];
         }
-        theOneBefore.next = theOneBefore.next.next;
         pos--;
     }
 
+
+
     @Override
     public void addAt(int index, int num) {
-
+        if(index < 0 || index >= pos)
+            throw new IndexOutOfBoundsException("trying to add at " + index);
+        makeRoom();
+        for (int i = pos; i > index ; i--) {
+            nums[i] = nums[i-1];
+        }
+        nums[index] = num;
+        pos++;
     }
 
     @Override
     public void set(int index, int num) {
-
+        if(index < 0 || index >= pos)
+            throw new IndexOutOfBoundsException("trying to set at " + index);
+        nums[index] = num;
     }
 
     @Override
     public int get(int index) {
-        return 0;
+        if(index < 0 || index >= pos)
+            throw new IndexOutOfBoundsException("trying to get at " + index);
+        return nums[index];
     }
 
     @Override
     public int indexOf(int num) {
-        return 0;
+        for (int i = 0; i < pos; i++) {
+            if(nums[i] == num)
+                return i;
+        }
+        return -1;
     }
 
     @Override
     public int[] toArray() {
-        return new int[0];
+        int[] temp = new int[pos];
+        for (int i = 0; i < pos; i++) {
+            temp[i] = nums[i];
+        }
+        return temp;
     }
 
     @Override
     public int size() {
-        return 0;
+        return pos;
     }
-
-    static class Link{
-        int value;
-        Link next;
-
-        public Link(int value){
-            this.value = value;
-        }
-    }
-
 
 }
