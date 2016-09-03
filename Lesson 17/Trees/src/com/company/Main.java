97253a84dc65af7023651355064b8e7e92abb999
Judaco <judacossa@gmail.com>
2016-09-03 16:35:18 +0300

    Trees
    Trees

--- Lesson 17/Trees/src/com/company/Main.java
+++ Lesson 17/Trees/src/com/company/Main.java
@@ -3,14 +3,18 @@ package com.company;
 public class Main {
 
         public static void main(String[] args) {
-            Node n1 = new Node(5);
+            /*Node n1 = new Node(5);
             Node n2 = new Node(100);
             Node n3 = new Node(500);
             Node n4 = new Node(250);
 
             n1.left = n2;
             n1.right = n3;
-            n3.left = n4;
+            n3.left = n4;*/
+
+            Node n1 = null;
+            Node n2 = null;
+            System.out.println(n1 == n2);
         }
 
         public static int max(Node node){
@@ -34,7 +38,13 @@ public class Main {
         }
 
         public static boolean compare(Node node1, Node node2){
-            return true;
+            if(node1 == node2)
+                return true;
+            if((node1 == null && node2 != null) || (node1 != null && node2 == null))
+                return false;
+            if(node1.value != node2.value)
+                return false;
+            return compare(node1.left, node2.left) && compare(node1.right, node2.right);
         }
     }
 
@@ -71,4 +81,39 @@ public class Main {
                 sum += right.sum();
             return sum;
         }
+
+
+        @Override
+        public boolean equals(Object obj) {
+            if(obj == null)
+                return false;
+            if(obj == this)
+                return true;
+            if(obj instanceof Node){
+                Node other = (Node)obj;
+                if(this.value != other.value)
+                    return false;
+                if(this.right != null){
+                    if(other.right == null)
+                        return false;
+                    if(!this.right.equals(other.right))
+                        return false;
+                }else{
+                    if(other.right != null)
+                        return false;
+                }
+                if(this.left != null){
+                    if(other.left == null)
+                        return false;
+                    if(!this.left.equals(other.left))
+                        return false;
+                }else{
+                    if(other.left != null)
+                        return false;
+                }
+                return true;
+            }
+            return false;
+        }
     }
+
